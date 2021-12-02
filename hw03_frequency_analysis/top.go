1b04cd283a8d4aaefd574bb5e1039f6ea93082f3
Oleksandr Anosov <alexanderanosov1996@gmail.com>
2021-12-02 08:26:18 +0200

    HW 3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,42 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
+
+func Top10(text string) []string {
+	wordCounters := make(map[string]int)
+	uniqueWords := []string{}
+
+	for _, word := range strings.Fields(text) {
+		word = strings.Trim(word, ".?!,:;-[]{}()\"")
+		word = strings.ToLower(word)
+		if len(word) == 0 {
+			continue
+		}
+
+		if _, found := wordCounters[word]; !found {
+			uniqueWords = append(uniqueWords, word)
+		}
+		wordCounters[word]++
+	}
+
+	sort.Slice(uniqueWords, func(i, j int) bool {
+		wordI := uniqueWords[i]
+		wordJ := uniqueWords[j]
+		if wordCounters[wordI] == wordCounters[wordJ] {
+			return wordI < wordJ
+		}
+		return wordCounters[wordI] > wordCounters[wordJ]
+	})
+
+	return uniqueWords[0:min(len(uniqueWords), 10)]
 }
