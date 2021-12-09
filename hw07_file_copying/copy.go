3b255f6a00de4654a6368a40794efc062b8a1ee8
Oleksandr Anosov <alexanderanosov1996@gmail.com>
2021-12-09 23:26:35 +0200

    HW 7

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -9,7 +13,51 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+func min(x, y int64) int64 {
+	if x < y {
+		return x
+	}
+	return y
+}
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	fromStat, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+
+	if fromStat.Size() == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fromStat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 {
+		limit = fromStat.Size() - offset
+	} else {
+		limit = min(fromStat.Size()-offset, limit)
+	}
+
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+
+	_, err = fromFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return nil
+	}
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	bar := pb.Full.Start64(limit)
+	defer bar.Finish()
+
+	_, err = io.CopyN(toFile, bar.NewProxyReader(fromFile), limit)
+	return err
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,21 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
 	// Place your code here.
+	t.Run("/dev/urandom is unsupported", func(t *testing.T) {
+		err := Copy("/dev/urandom", "/tmp/urandom", 0, 0)
+		require.Equal(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("nonexistent source file", func(t *testing.T) {
+		err := Copy("nonexistent", "/tmp/f", 0, 0)
+		require.ErrorIs(t, err, os.ErrNotExist)
+	})
 }
