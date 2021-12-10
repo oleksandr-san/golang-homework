6250f1724fc25e1491be1ecd37ece13070cf4146
Oleksandr Anosov <alexanderanosov1996@gmail.com>
2021-12-10 16:37:11 +0200

    Fix lint error

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -8,11 +8,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func requireFilesEqual(t *testing.T, expectedPath, actualPath string) {
+const tempFile = "/tmp/out"
+
+func requireEqualToTmp(t *testing.T, expectedPath string) {
+	t.Helper()
+
 	expected, err := ioutil.ReadFile(expectedPath)
 	require.Nil(t, err)
 
-	actual, err := ioutil.ReadFile(actualPath)
+	actual, err := ioutil.ReadFile(tempFile)
 	require.Nil(t, err)
 
 	require.Equal(t, expected, actual)
@@ -20,59 +24,59 @@ func requireFilesEqual(t *testing.T, expectedPath, actualPath string) {
 
 func TestCopy(t *testing.T) {
 	t.Run("nonexistent source file", func(t *testing.T) {
-		err := Copy("nonexistent", "/tmp/out", 0, 0)
+		err := Copy("nonexistent", tempFile, 0, 0)
 		require.ErrorIs(t, err, os.ErrNotExist)
 	})
 
 	t.Run("offset can not be larger than source size", func(t *testing.T) {
-		err := Copy("testdata/input.txt", "/tmp/out", 1000000, 0)
+		err := Copy("testdata/input.txt", tempFile, 1000000, 0)
 		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
 	})
 
 	t.Run("files with unknown size are unsupported", func(t *testing.T) {
-		err := Copy("/dev/urandom", "/tmp/out", 0, 0)
+		err := Copy("/dev/urandom", tempFile, 0, 0)
 		require.Equal(t, err, ErrUnsupportedFile)
 	})
 
 	t.Run("whole file copy", func(t *testing.T) {
-		err := Copy("testdata/input.txt", "/tmp/out", 0, 0)
+		err := Copy("testdata/input.txt", tempFile, 0, 0)
 
 		require.Nil(t, err)
-		requireFilesEqual(t, "testdata/out_offset0_limit0.txt", "/tmp/out")
+		requireEqualToTmp(t, "testdata/out_offset0_limit0.txt")
 	})
 
 	t.Run("file copy (offset 0, limit 10)", func(t *testing.T) {
-		err := Copy("testdata/input.txt", "/tmp/out", 0, 10)
+		err := Copy("testdata/input.txt", tempFile, 0, 10)
 
 		require.Nil(t, err)
-		requireFilesEqual(t, "testdata/out_offset0_limit10.txt", "/tmp/out")
+		requireEqualToTmp(t, "testdata/out_offset0_limit10.txt")
 	})
 
 	t.Run("file copy (offset 0, limit 1000)", func(t *testing.T) {
-		err := Copy("testdata/input.txt", "/tmp/out", 0, 1000)
+		err := Copy("testdata/input.txt", tempFile, 0, 1000)
 
 		require.Nil(t, err)
-		requireFilesEqual(t, "testdata/out_offset0_limit1000.txt", "/tmp/out")
+		requireEqualToTmp(t, "testdata/out_offset0_limit1000.txt")
 	})
 
 	t.Run("file copy (offset 0, limit 10000)", func(t *testing.T) {
-		err := Copy("testdata/input.txt", "/tmp/out", 0, 10000)
+		err := Copy("testdata/input.txt", tempFile, 0, 10000)
 
 		require.Nil(t, err)
-		requireFilesEqual(t, "testdata/out_offset0_limit10000.txt", "/tmp/out")
+		requireEqualToTmp(t, "testdata/out_offset0_limit10000.txt")
 	})
 
 	t.Run("file copy (offset 100, limit 1000)", func(t *testing.T) {
-		err := Copy("testdata/input.txt", "/tmp/out", 100, 1000)
+		err := Copy("testdata/input.txt", tempFile, 100, 1000)
 
 		require.Nil(t, err)
-		requireFilesEqual(t, "testdata/out_offset100_limit1000.txt", "/tmp/out")
+		requireEqualToTmp(t, "testdata/out_offset100_limit1000.txt")
 	})
 
 	t.Run("file copy (offset 6000, limit 1000)", func(t *testing.T) {
-		err := Copy("testdata/input.txt", "/tmp/out", 6000, 1000)
+		err := Copy("testdata/input.txt", tempFile, 6000, 1000)
 
 		require.Nil(t, err)
-		requireFilesEqual(t, "testdata/out_offset6000_limit1000.txt", "/tmp/out")
+		requireEqualToTmp(t, "testdata/out_offset6000_limit1000.txt")
 	})
 }
